a8100b86977877de6c938cb86069ea5ade7b2a35
bafinck <b.finck@etu.unistra.fr>
2021-11-26 15:52:07 +0100

    front

--- front/src/api/questions.js
+++ front/src/api/questions.js
@@ -1,34 +1,52 @@
-import { host, checkStatus } from ".";
+import { host, checkStatus } from '.';
 
 const questions = {
   addQuestion: (body) => {
+    let formData = new FormData();
+
+    if (body.author !== undefined) {
+      formData.append('author', body.author);
+    }
+
+    formData.append('type', body.type);
+    formData.append('question', body.question);
+
+    if (body.file !== null && body.file.length === 1) {
+      formData.append('file', body.file[0]);
+    }
+
     return fetch(`${host}/questions`, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(body)
-    }).then(checkStatus).then(res => res.json())
+      method: 'POST',
+      body: formData,
+    })
+      .then(checkStatus)
+      .then((res) => res.json());
   },
   getAll: () => {
     return fetch(`${host}/questions`, {
-      method: "GET"
-    }).then(checkStatus).then(res => res.json())
+      method: 'GET',
+    })
+      .then(checkStatus)
+      .then((res) => res.json());
   },
   getOne: (id) => {
     return fetch(`${host}/questions/${id}`, {
-      method: "GET"
-    }).then(checkStatus).then(res => res.json())
+      method: 'GET',
+    })
+      .then(checkStatus)
+      .then((res) => res.json());
   },
-  answer: ({id, body}) => {
+  answer: ({ id, body }) => {
     return fetch(`${host}/questions/${id}/answer`, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "content-type": "application/json"
+        'content-type': 'application/json',
       },
-      body: JSON.stringify(body)
-    }).then(checkStatus).then(res => res.json())
-  }
-}
+      body: JSON.stringify(body),
+    })
+      .then(checkStatus)
+      .then((res) => res.json());
+  },
+};
 
 export default questions;
--- front/src/page/Question.js
+++ front/src/page/Question.js
@@ -1,58 +1,90 @@
-import {useState } from "react"
+import { useState } from 'react';
 
-import Container from "react-bootstrap/Container"
-import Form from "react-bootstrap/Form"
-import Button from "react-bootstrap/Button"
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
-import { useMutation } from "react-query"
-import { useHistory } from "react-router"
-import api from "../api"
+import { useMutation } from 'react-query';
+import { useHistory } from 'react-router';
+import api from '../api';
 
 export default function Question() {
-  const history = useHistory()
+  const history = useHistory();
 
-  const [author, setAuthor] = useState("")
-  const [type, setType] = useState("question")
-  const [question, setQuestion] = useState("")
+  const [author, setAuthor] = useState('');
+  const [type, setType] = useState('question');
+  const [question, setQuestion] = useState('');
+  const [file, setFile] = useState(null);
 
-  const {mutate: addQuestion} = useMutation(api.questions.addQuestion, {
+  const { mutate: addQuestion } = useMutation(api.questions.addQuestion, {
     onSuccess: (data) => {
-      history.push("/")
-    }
-  })
+      history.push('/');
+    },
+  });
 
   function handleAdd() {
     if (!!question) {
       addQuestion({
-        author: !!author ? author: undefined,
+        author: !!author ? author : undefined,
         type,
-        question
-      })
+        question,
+        file,
+      });
     }
   }
 
-  return <Container>
-    <h1>Poser une question</h1>
-    
-    <Form.Group>
-      <Form.Label>Votre nom</Form.Label>
-      <Form.Control value={author} onChange={(e) => setAuthor(e.target.value)} />
-    </Form.Group>
+  return (
+    <Container>
+      <h1>Poser une question</h1>
 
-    <Form.Group>
-      <Form.Label>Type</Form.Label>
-      <Form.Control as="select" value={type} onChange={e => setType(e.target.value)}>
-        <option value="question">Question</option>
-        <option value="action">Action</option>
-      </Form.Control>  
-    </Form.Group>
+      <Form.Group>
+        <Form.Label>Votre nom</Form.Label>
+        <Form.Control
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+      </Form.Group>
 
-    <Form.Group>
-      <Form.Label>Question/Action</Form.Label>
-      <Form.Control as="textarea" value={question} onChange={(e) => setQuestion(e.target.value)} isInvalid={question === ""} rows={5}/>
-      
-    </Form.Group>
+      <Form.Group>
+        <Form.Label>Type</Form.Label>
+        <Form.Control
+          as="select"
+          value={type}
+          onChange={(e) => setType(e.target.value)}
+        >
+          <option value="question">Question</option>
+          <option value="action">Action</option>
+        </Form.Control>
+      </Form.Group>
 
-    <Button onClick={handleAdd} disabled={!question} style={{marginTop: "10px"}}>Ajouter</Button>
-  </Container>
+      <Form.Group>
+        <Form.Label>Question/Action</Form.Label>
+        <Form.Control
+          as="textarea"
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+          isInvalid={question === ''}
+          rows={5}
+        />
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Ajouter une image</Form.Label>
+        <br />
+        <Form.Control
+          type="file"
+          accept=".jpg,.png"
+          onChange={(e) => setFile(e.target.files)}
+        />
+      </Form.Group>
+
+      <Button
+        onClick={handleAdd}
+        disabled={!question}
+        style={{ marginTop: '10px' }}
+      >
+        Ajouter
+      </Button>
+    </Container>
+  );
 }
