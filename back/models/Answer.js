f2266253fcadcb0e56bc62c2a716198b0a47771b
bafinck <b.finck@etu.unistra.fr>
2021-12-10 14:41:57 +0100

    comit

--- back/models/Answer.js
+++ back/models/Answer.js
@@ -1,9 +1,14 @@
 module.exports = (sequelize, DataTypes) => {
-  const Answer = sequelize.define('Answer', {
-    answer: DataTypes.STRING
-  }, {});
-  Answer.associate = function(models) {
-    models.Answer.belongsTo(models.Question);
-  };
-  return Answer;
+	const Answer = sequelize.define(
+		"Answer",
+		{
+			answer: DataTypes.STRING,
+		},
+		{}
+	);
+	Answer.associate = function (models) {
+		models.Answer.belongsTo(models.Question);
+	};
+	return Answer;
 };
+//d
